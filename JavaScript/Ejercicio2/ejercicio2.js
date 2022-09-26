3956609c86af522cec7e1005bc3112bca17a379b
Wicho <luiscalderonmiranda@gmail.com>
2022-09-25 21:53:08 -0500

    Ejercicio 2

---
+++ JavaScript/Ejercicio2/ejercicio2.js
@@ -0,0 +1,13 @@
+const yo = [
+  "Luis Eduardo Calderon Miranda",
+  21,
+  true,
+  new Date(2001, 8, 13),
+  libroFavorito = {
+    titulo: "Lazarillo de Tormes",
+    autor: "Anonimo", 
+    fecha: new Date(1551),
+    url: "https://www.amazon.com.mx/Lazarillo-Tormes-critica-historico-Spanish/dp/6071411122/ref=sr_1_1?__mk_es_MX=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=9MYMYAUWIXHJ&keywords=lazarillo+de+tormes&qid=1664160609&sprefix=lazarillo+de+torme%2Caps%2C280&sr=8-1",  
+  } ];
+
+console.log(yo);
