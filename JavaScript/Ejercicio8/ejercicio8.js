3a654ea93103cfdb4e4855050dc1a55ee7d50e86
Wicho <luiscalderonmiranda@gmail.com>
2022-10-02 21:38:09 -0500

    Ejercicio 8 completado

--- JavaScript/Ejercicio8/ejercicio8.js
+++ JavaScript/Ejercicio8/ejercicio8.js
@@ -7,10 +7,26 @@ console.log(siempreTrue());
 
 //- Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
 
-const promesa = new Promise((resolve, reject) => {
-  setTimeout(5000);
-  console.log("Hola soy una promesa");
+const promesa = new Promise((resolve) => {
+  setTimeout(() => console.log("Soy una promesa"), 5000);
   resolve();
 })
 
-promesa();
+promesa;
+
+//- Una función generadora de índices pares automáticos
+function* generaPares() {
+  let indice = 0;
+  while(true) {
+    indice += 2;
+    yield indice;
+  }
+}
+
+const generarPar = generaPares();
+
+console.log(generarPar.next().value);
+console.log(generarPar.next().value);
+console.log(generarPar.next().value);
+console.log(generarPar.next().value);
+console.log(generarPar.next().value);
