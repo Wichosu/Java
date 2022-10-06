f5ea4d5975a8e2e82c0f5230f78aa4b7c6518e34
Wicho <luiscalderonmiranda@gmail.com>
2022-10-05 21:06:01 -0500

    Ejercicio 12 completado

--- JavaScript/Ejercicio12/ejercicio12.js
+++ JavaScript/Ejercicio12/ejercicio12.js
@@ -1,18 +1,11 @@
-let lista = [];
 function fibonacci(num) {
-  if (num <= 1) return num;
-  lista = [...[(parseInt(fibonacci(num-1)) + parseInt(fibonacci(num-2)))]];
-  return lista;
+  if (num === 1) return [1];
+  if (num === 2) return [1, 1];
+  let list = [1, 1];
+  for(let i = 2; i < num; i++) {
+    list.push(list[i - 1] + list[i - 2]);
+  }
+  return list;
 }
-  
-let fibo = fibonacci(6);
 
-let array = [1, 2 ,3];
-
-let numero = 2
-
-let otro = 4
-
-let algo = [...[numero], otro];
-
-console.log([...array]);
+console.log(fibonacci(6));
