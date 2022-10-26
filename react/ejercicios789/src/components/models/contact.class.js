8fb0f76697c78911668647ee1f75409b94b73636
Wicho <luiscalderonmiranda@gmail.com>
2022-10-25 19:54:03 -0500

    contact class doen

---
+++ react/ejercicios789/src/components/models/contact.class.js
@@ -0,0 +1,13 @@
+export class Contact {
+  name = '';
+  surname = '';
+  number = '';
+  email = '';
+
+  constructor(name, surname, number, email) {
+    this.name = name;
+    this.surname = surname;
+    this.number = number;
+    this.email = email;
+  }
+}
