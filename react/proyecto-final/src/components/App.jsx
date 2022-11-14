ac363a10dc615b7719dbc256f1cc1461416f3964
Wicho <54341712+Wichosu@users.noreply.github.com>
2022-11-13 20:09:52 -0600

    React basico terminado

---
+++ react/proyecto-final/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+ * Función Anónima para crear un Componente principal
+ * @returns {React.Component} Componente principal de nuestra aplicación
+ */
+
+// NODE_ENV.BASE_URL
+
+const App = () => {
+  return (
+    <div>
+      <h1>Proyecto Final</h1>
+    </div>
+  );
+};
+
+export default App;
